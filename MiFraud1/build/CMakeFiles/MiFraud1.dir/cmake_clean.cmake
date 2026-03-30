dd46b2516a6af4d5e81185e3e53193d9d20a5228
Rodoplax <153656514+Rodoplax@users.noreply.github.com>
2026-03-30 19:08:16 +0200

    Add files via upload

---
+++ MiFraud1/build/CMakeFiles/MiFraud1.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiFraud1.dir/src/Clustering.cpp.o"
+  "CMakeFiles/MiFraud1.dir/src/Clustering.cpp.o.d"
+  "CMakeFiles/MiFraud1.dir/src/Location.cpp.o"
+  "CMakeFiles/MiFraud1.dir/src/Location.cpp.o.d"
+  "CMakeFiles/MiFraud1.dir/src/VectorInt.cpp.o"
+  "CMakeFiles/MiFraud1.dir/src/VectorInt.cpp.o.d"
+  "CMakeFiles/MiFraud1.dir/src/VectorLocation.cpp.o"
+  "CMakeFiles/MiFraud1.dir/src/VectorLocation.cpp.o.d"
+  "CMakeFiles/MiFraud1.dir/src/main.cpp.o"
+  "CMakeFiles/MiFraud1.dir/src/main.cpp.o.d"
+  "MiFraud1"
+  "MiFraud1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MiFraud1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MiFraud1/build/CMakeFiles/MiFraud1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MiFraud1.
+# This may be replaced when dependencies are built.
